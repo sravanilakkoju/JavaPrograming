77c7e40150452a33a966b16453b5cf5c0d068894
sravanilakkoju <67959721+sravanilakkoju@users.noreply.github.com>
2023-08-16 16:39:49 +0530

    Create newclass.java

---
+++ newclass.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class newclass {
+    public void myClass(String name){
+        System.out.println("im is the first new class");
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        System.out.println(s + name);
+    }
+    public void urClass(){
+        System.out.println("this is the second class");
+        int[] array = {1,2,3,4};
+        for(int i = 0;i< array.length;i++){
+            System.out.println(array[i]);
+        }
+
+    }
+}

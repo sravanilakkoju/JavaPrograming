71ff0133a2c53f9609dc4b9e1961f1deb909310b
sravanilakkoju <67959721+sravanilakkoju@users.noreply.github.com>
2023-08-16 16:38:40 +0530

    Create Practice.java

---
+++ Practice.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class App{
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+
+        //calling te new class into te main class as an object...
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        newclass newClassObject = new newclass();
+        newClassObject.myClass(name);
+
+        //calling the second new class imto the main class as anobject...
+        newclass newClassObject1 = new newclass();
+        newClassObject1.urClass();
+
+    }
+}
